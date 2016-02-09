7839346794c7f2b5f20215108756f21c4c0e3b7a
Anton Klimenko <anton.klimenko@evrythng.com>
2016-02-09 21:43:30 +0300

    Assignment bug fixes

--- src/main/java/part1/week1/quickunion/assignment/Percolation.java
+++ src/main/java/part1/week1/quickunion/assignment/Percolation.java
@@ -1,108 +1,106 @@
-/*
- * (c) Copyright 2016 EVRYTHNG Ltd London / Zurich
- * www.evrythng.com
- */
 package part1.week1.quickunion.assignment;
 
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.stream.IntStream;
-
 public class Percolation {
 
-	private final int N;
-	private final int top;
-	private final int bottom;
-	private final int[] sites;
-	private final WeightedQuickUnionUF unionFind;
+    private final int N;
+    private final int top;
+    private final int bottom;
+    private final int[] sites;
+    private final WeightedQuickUnionUF unionFind;
 
-	public Percolation(int N) {
+    public Percolation(int N) {
 
-		if (N < 0) {
-			throw new IllegalArgumentException("N < 0");
-		}
-		this.N = N;
-		this.top = 0;
-		this.bottom = N * N + 1;
+        if (N < 1) {
+            throw new IllegalArgumentException("N < 1");
+        }
+        this.N = N;
+        this.top = 0;
+        this.bottom = N * N + 1;
 
-		this.unionFind = new WeightedQuickUnionUF(N * N + 2);
-		this.sites = IntStream.iterate(0, i -> 0).limit(N * N + 2).toArray();
-		this.sites[top] = 1;
-		this.sites[bottom] = 1;
-	}
+        this.unionFind = new WeightedQuickUnionUF(N * N + 2);
+        this.sites = new int[N * N + 2];
+        this.sites[top] = 1;
+        this.sites[bottom] = 1;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int N = 1000;
-		Percolation p = new Percolation(N);
-		int opened = 0;
-		while (!p.percolates()) {
-			int i = StdRandom.uniform(1, N + 1);
-			int j = StdRandom.uniform(1, N + 1);
+        int N = 100;
+        Percolation p = new Percolation(N);
+        int opened = 0;
+        while (!p.percolates()) {
+            int i = StdRandom.uniform(1, N + 1);
+            int j = StdRandom.uniform(1, N + 1);
 
-			if (!p.isOpen(i, j)) {
-				p.open(i, j);
-				opened++;
-			}
-		}
-		System.out.println("p.percolationProbability() = " + (double) opened / (N * N));
-	}
+            if (!p.isOpen(i, j)) {
+                p.open(i, j);
+                opened++;
+            }
+        }
+        System.out.println("p.percolationProbability() = " + (double) opened / (N * N));
+    }
 
-	public void open(final int i, final int j) {
+    public void open(final int i, final int j) {
 
-		if (i < 1 || i > N || j < 1 || j > N) {
-			throw new IndexOutOfBoundsException(String.format("i: %d, j: %d must belong [1..%d]", i, j, this.N));
-		}
+        if (i < 1 || i > N || j < 1 || j > N) {
+            throw new IndexOutOfBoundsException(String.format("i: %d, j: %d must belong [1..%d]", i, j, this.N));
+        }
 
-		if (isOpen(i, j)) {
-			return;
-		}
+        if (isOpen(i, j)) {
+            return;
+        }
 
-		int siteIndex = index(i, j);
-		this.sites[siteIndex] = 1;
+        int siteIndex = index(i, j);
+        this.sites[siteIndex] = 1;
 
-		if (i == 1) { // top
-			unionFind.union(top, siteIndex);
-		} else if (i == N) { // bottom
-			unionFind.union(bottom, siteIndex);
-		}
-		if (i > 1 && isOpen(i - 1, j)) {  // up
-			unionFind.union(siteIndex, index(i - 1, j));
-		}
-		if (i < N && isOpen(i + 1, j)) { // down
-			unionFind.union(siteIndex, index(i + 1, j));
-		}
-		if (j > 1 && isOpen(i, j - 1)) { // left
-			unionFind.union(siteIndex, index(i, j - 1));
-		}
-		if (j < N && isOpen(i, j + 1)) { // right
-			unionFind.union(siteIndex, index(i, j + 1));
-		}
-	}
+        if (i == 1) { // top
+            unionFind.union(top, siteIndex);
+        } else if (i == N) { // bottom
+            unionFind.union(bottom, siteIndex);
+        }
+        if (i > 1 && isOpen(i - 1, j)) {  // up
+            unionFind.union(siteIndex, index(i - 1, j));
+        }
+        if (i < N && isOpen(i + 1, j)) { // down
+            unionFind.union(siteIndex, index(i + 1, j));
+        }
+        if (j > 1 && isOpen(i, j - 1)) { // left
+            unionFind.union(siteIndex, index(i, j - 1));
+        }
+        if (j < N && isOpen(i, j + 1)) { // right
+            unionFind.union(siteIndex, index(i, j + 1));
+        }
+    }
 
-	private int index(final int i, final int j) {
+    private int index(final int i, final int j) {
 
-		return (i - 1) * N + j;
-	}
+        return (i - 1) * N + j;
+    }
 
-	public boolean isOpen(final int i, final int j) {
+    public boolean isOpen(final int i, final int j) {
 
-		if (i < 1 || i > N || j < 1 || j > N) {
-			throw new IndexOutOfBoundsException(String.format("i: %d, j: %d must belong [1..%d]", i, j, this.N));
-		}
+        if (i < 1 || i > N || j < 1 || j > N) {
+            throw new IndexOutOfBoundsException(String.format("i: %d, j: %d must belong [1..%d]", i, j, this.N));
+        }
 
-		return this.sites[index(i, j)] == 1;
-	}
+        return this.sites[index(i, j)] == 1;
+    }
 
-	public boolean isFull(final int i, final int j) {
+    public boolean isFull(final int i, final int j) {
 
-		return unionFind.connected(index(i, j), top);
-	}
+        if (i < 1 || i > N || j < 1 || j > N) {
+            throw new IndexOutOfBoundsException(String.format("i: %d, j: %d must belong [1..%d]", i, j, this.N));
+        }
 
-	public boolean percolates() {
+        return isOpen(i, j) && unionFind.connected(index(i, j), top);
+    }
 
-		return unionFind.connected(top, bottom);
-	}
+    public boolean percolates() {
+
+        return unionFind.connected(top, bottom);
+    }
 
 }
--- src/main/java/part1/week1/quickunion/assignment/PercolationStats.java
+++ src/main/java/part1/week1/quickunion/assignment/PercolationStats.java
@@ -1,8 +1,3 @@
-/*
- * (c) Copyright 2016 EVRYTHNG Ltd London / Zurich
- * www.evrythng.com
- */
-
 package part1.week1.quickunion.assignment;
 
 import edu.princeton.cs.algs4.StdRandom;
@@ -10,66 +5,66 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private final double[] probabilities;
-	private final int T;
+    private final double[] probabilities;
+    private final int T;
 
-	public PercolationStats(final int N, final int T) {
+    public PercolationStats(final int N, final int T) {
 
-		if (N < 0 || T < 0) {
-			throw new IllegalArgumentException("N and T args can not be negative");
-		}
+        if (N < 1 || T < 1) {
+            throw new IllegalArgumentException("N and T args can not be negative");
+        }
 
-		this.T = T;
-		this.probabilities = new double[T];
+        this.T = T;
+        this.probabilities = new double[T];
 
-		for (int k = 0; k < T; k++) {
-			Percolation p = new Percolation(N);
-			int opened = 0;
-			while (!p.percolates()) {
-				int i = StdRandom.uniform(1, N + 1);
-				int j = StdRandom.uniform(1, N + 1);
+        for (int k = 0; k < T; k++) {
+            Percolation p = new Percolation(N);
+            int opened = 0;
+            while (!p.percolates()) {
+                int i = StdRandom.uniform(1, N + 1);
+                int j = StdRandom.uniform(1, N + 1);
 
-				if (!p.isOpen(i, j)) {
-					p.open(i, j);
-					opened++;
-				}
-			}
-			this.probabilities[k] = (double) opened / (N * N);
-		}
-	}
+                if (!p.isOpen(i, j)) {
+                    p.open(i, j);
+                    opened++;
+                }
+            }
+            this.probabilities[k] = (double) opened / (N * N);
+        }
+    }
 
-	public double mean() {
+    public double mean() {
 
-		return StdStats.mean(this.probabilities);
-	}
+        return StdStats.mean(this.probabilities);
+    }
 
-	public double stddev() {
+    public double stddev() {
 
-		return StdStats.stddev(this.probabilities);
-	}
+        return StdStats.stddev(this.probabilities);
+    }
 
-	public double confidenceLo() {
+    public double confidenceLo() {
 
-		return mean() - (1.96 * stddev()) / Math.sqrt(this.T);
-	}
+        return mean() - (1.96 * stddev()) / Math.sqrt(this.T);
+    }
 
-	public double confidenceHi() {
+    public double confidenceHi() {
 
-		return mean() + (1.96 * stddev()) / Math.sqrt(this.T);
-	}
+        return mean() + (1.96 * stddev()) / Math.sqrt(this.T);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		if (args.length != 2) {
-			throw new IllegalArgumentException("Expected 2 args: N and T");
-		}
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Expected 2 args: N and T");
+        }
 
-		int N = Integer.valueOf(args[0]);
-		int T = Integer.valueOf(args[1]);
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
 
-		PercolationStats ps = new PercolationStats(N, T);
-		System.out.println("mean\t\t\t\t\t= " + ps.mean());
-		System.out.println("stddev\t\t\t\t\t= " + ps.stddev());
-		System.out.println("95% confidence interval\t= " + ps.confidenceLo() + ", " + ps.confidenceHi());
-	}
+        PercolationStats ps = new PercolationStats(N, T);
+        System.out.println("mean\t\t\t\t\t= " + ps.mean());
+        System.out.println("stddev\t\t\t\t\t= " + ps.stddev());
+        System.out.println("95% confidence interval\t= " + ps.confidenceLo() + ", " + ps.confidenceHi());
+    }
 }
