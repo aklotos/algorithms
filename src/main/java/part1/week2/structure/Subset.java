9ffff4007317612dc13c68906c52b5c684e1b4d4
Anton Klimenko <anton.klimenko@evrythng.com>
2016-02-18 03:45:19 +0300

    Added implementation of Subset
    master

---
+++ src/main/java/part1/week2/structure/Subset.java
@@ -0,0 +1,20 @@
+
+package part1.week2.structure;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Subset {
+	public static void main(String[] args) {
+		final int k = Integer.parseInt(args[0]);
+		String[] items = StdIn.readLine().split("\\p{javaWhitespace}+");
+		final int N = items.length;
+
+		RandomizedQueue<String> queue = new RandomizedQueue<>();
+		for (String item: items) {
+			queue.enqueue(item);
+		}
+		for (int i = 0; i < k; i++) {
+			System.out.println(queue.dequeue());
+		}
+	}
+}
